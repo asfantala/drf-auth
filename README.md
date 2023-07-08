d943741386ffa7071de78f60484e6c9e363ff53a
asfantala <22037049@student.ltuc.com>
2023-07-08 10:43:51 +0000

    lab33

--- README.md
+++ README.md
@@ -1,11 +1,58 @@
-# Django_Docker - LAB - Class 32
-## Project:Permissions & Postgresql
+# Django_Docker - LAB - Class 33
+## Project: Authentication & Production Server
 ### Author: Tala Asfan
+
+### Installation 
+
+1. Create and activate a virtual environment:
+```
+ python -m venv venv 
+ source venv/bin/activate
+```
+2. Install the project dependencies:
+```
+pip install -r requirements.txt
+
+```
 ### Setup
 - you need to have docker installed on your machine
+### Features - Docker
+- Switch from Django's development server to using Gunicorn.
+   - pip install gunicorn 
+   - edit the command inside the docker-compose.yml to this :
+    > gunicorn myapp.wsgi:application --bind 0.0.0.0:8000 --workers 4
+   - To handle static files on the Django side using Whitenoise.
+        - add a middleware in sitting.py:     'whitenoise.middleware.WhiteNoiseMiddleware',
+        - in settings.py : 
+        ```
+        STATIC_DIR = os.path.join(BASE_DIR, 'static')
+        STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+        STATIC_URL = 'static/'
+        STATICFILES_DIRS = [
+        STATIC_DIR,
+        ]
+        ```
+        - run command : python manage.py collectstatic
 
 ### How to initialize/run your application
 - docker compose up
+
+
+### To manually test the API using an HTTP client such as HTTPie or ThunderClient, follow these steps:
+
+- Get Tokens:
+
+    - URL: /api/token/
+    - Method: POST
+    - Token: Not required
+    - Description: Send a POST request to this endpoint with the appropriate credentials to obtain access and refresh tokens.
+
+- Refresh Tokens:
+
+    - URL: /api/token/refresh/
+    - Method: POST
+    - Token: Refresh token required
+    - Description: Send a POST request to this endpoint with a valid refresh token to obtain a new access token.    
 ### Tests
 - python manage.py test
 
--- items/tests.py
+++ items/tests.py
@@ -1,22 +1,91 @@
-from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
 from .models import Item
 
 
-class ItemTests(TestCase):
+class ItemTests(APITestCase):
     @classmethod
     def setUpTestData(cls):
-        testuser1 = get_user_model().objects.create_user(username='testuser1', password='pass')
+        testuser1 = get_user_model().objects.create_user(
+            username="testuser1", password="pass"
+        )
         testuser1.save()
 
-        test_item = Item.objects.create(user=testuser1, name='flower', description='test desc ...')
-        test_item.save()
+        test_Item = Item.objects.create(
+            name="rake",
+            owner=testuser1,
+            description="Better for collecting leaves than a shovel.",
+        )
+        test_Item.save()
 
-    def test_item_model(self):
+    # class 32
+    def setUp(self):
+        self.client.login(username="testuser1", password="pass")
+
+    def test_items_model(self):
         item = Item.objects.get(id=1)
         actual_user = str(item.user)
         actual_name = str(item.name)
         actual_desc = str(item.description)
         self.assertEqual(actual_user, "testuser1")
-        self.assertEqual(actual_name, "flower")
-        self.assertEqual(actual_desc, "test desc ...")
+        self.assertEqual(actual_name, "rake")
+        self.assertEqual(
+            actual_desc, "Better for collecting leaves than a shovel."
+        )
+
+    def test_get_items_list(self):
+        url = reverse("items_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        items = response.data
+        self.assertEqual(len(items), 1)
+        self.assertEqual(items[0]["name"], "rake")
+
+    def test_get_item_by_id(self):
+        url = reverse("item_detail", args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        item = response.data
+        self.assertEqual(item["name"], "rake")
+
+    def test_create_item(self):
+        url = reverse("items_list")
+        data = {"user": 1, "name": "spoon", "description": "good for cereal and soup"}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        items = Item.objects.all()
+        self.assertEqual(len(items), 2)
+        self.assertEqual(items.objects.get(id=2).name, "spoon")
+
+    def test_update_thing(self):
+        url = reverse("item_detail", args=(1,))
+        data = {
+            "user": 1,
+            "name": "rake",
+            "description": "pole with a crossbar toothed like a comb.",
+        }
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        thing = Item.objects.get(id=1)
+        self.assertEqual(thing.name, data["name"])
+        self.assertEqual(thing.user.id, data["user"])
+        self.assertEqual(thing.description, data["description"])
+
+    def test_delete_item(self):
+        url = reverse("item_detail", args=(1,))
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        item = Item.objects.all()
+        self.assertEqual(len(item), 0)
+
+    # class 32
+    def test_authentication_required(self):
+        self.client.logout()
+        url = reverse("items_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
